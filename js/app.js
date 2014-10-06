414dff294723e0e700d76ef917cf56bc29a8c665
prufrock123 <shawnmllr86@gmail.com>
2014-10-06 12:57:01 -0500

    lays the JS foundation.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-// document.querySelector(body)
+window.onload = app;
+
+function app() {
+    "use strict";
+
+    // var backGround = document.querySelector("body")
+
+    // setInterval(function() {
+    //     backGround.style["background-color"] = "red"
+    // }, 2000);
+
+	
+	var 
+
+    setInterval(count, 1000) {}
+}
